6fc787e0ed40f516b8b545dfbd69cbf236aae913
Red <vhanchar@ucsc.edu>
2019-07-16 11:59:38 -0700

    exporting numbers from data.js to Numbers.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import React from "react";
+import Numbers from "./components/ButtonComponents/NumberButtons/Numbers";
+
+
 import "./App.css";
 // STEP 4 - import the button and display components
 // Don't forget to import any extra css/scss files you build into the correct component
@@ -15,6 +18,7 @@ function App() {
 
   return (
     <div className="container">
+      <Numbers />
       <Logo />
       <div className="App">
         {/* STEP 4 - Render your components here and be sure to properly import/export all files */}
--- src/components/ButtonComponents/NumberButtons/Numbers.js
+++ src/components/ButtonComponents/NumberButtons/Numbers.js
@@ -1,16 +1,21 @@
-import React from "react";
-
 //import any components needed
+import React from "react";
+import {numbers} from "/Users/red/Desktop/lambda/projects/lambda-calculator/src/data"
+
+
 
 //Import your array data to from the provided data file
-
 const Numbers = () => {
+  console.log('PRINTING NUMBERS ' + {numbers});
   // STEP 2 - add the imported data to state
   return (
     <div>
+      
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
        it any props needed by the child component*/}
     </div>
   );
 };
+
+export default Numbers;
