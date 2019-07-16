dbcb5fbdd3ff9c442f58ad9e39fd0ac0dfda3988
Red <vhanchar@ucsc.edu>
2019-07-16 14:40:47 -0700

    exported the rest of the values for data.js

--- src/data.js
+++ src/data.js
@@ -7,11 +7,7 @@
 export const numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "."];
 
 
-
-
-
-
-const operators = [
+export const operators = [
   {
     char: "/",
     value: "/"
@@ -34,4 +30,4 @@ const operators = [
   }
 ];
 
-const specials = ["C", "+/-", "%"];
+export const specials = ["C", "+/-", "%"];
