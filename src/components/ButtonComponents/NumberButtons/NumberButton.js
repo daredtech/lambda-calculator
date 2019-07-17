986b89473a33d05a8e0171f5903c20a60d5fadc2
Red <vhanchar@ucsc.edu>
2019-07-16 19:49:55 -0700

    finished mvp

--- src/components/ButtonComponents/NumberButtons/NumberButton.js
+++ src/components/ButtonComponents/NumberButtons/NumberButton.js
@@ -3,9 +3,9 @@ import React from "react";
 const NumberButton = (props) => {
   const {number} = props;
   return (
-    <div lassName="numberButton">
+    <div className="numberButton">
       {/* Display a button element rendering the data being passed down from the parent container on props */}
-      <button> {number} </button>
+      <button className={number}> {number} </button>
     </div>
   );
 };
--- src/components/ButtonComponents/OperatorButtons/OperatorButton.js
+++ src/components/ButtonComponents/OperatorButtons/OperatorButton.js
@@ -3,7 +3,7 @@ import React from "react";
 const OperatorButton = (pros) => {
   const {operator} = pros;
   return (
-    <div nameClass='operatorButton'>
+    <div className='operatorButton'>
       {/* Display a button element rendering the data being passed down from the parent container on props */}
       <button> {operator} </button>
     </div>
